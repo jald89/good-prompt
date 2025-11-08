053671ddeba68ba03bb1c8419aff3d6103a6cf74
Jhonathan <jald89@gmail.com>
2025-11-08 12:45:57 +0100

    Enable CORS headers for API requests

--- src/server.js
+++ src/server.js
@@ -4,6 +4,18 @@ const analyzeRoutes = require('./routes/analyzeRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
+
 app.use(express.json({ limit: '1mb' }));
 
 app.get('/', (_req, res) => {
